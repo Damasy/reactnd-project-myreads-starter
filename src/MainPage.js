8b6cac696b51e6a38771d234063a7efc415a8260
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-04-04 16:15:21 +0200

    base route functionalities

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import BookSearch from './BookSearch'
+import BookShelf from './BookShelf'
 
 export default class MainPage extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="app">
+        <Route path="/search" render={
+          () => (<BookSearch/>)
+        } />
+        <Route exact path="/" render={
+          () => (<BookShelf/>)
+        } />
       </div>
     )
   }
